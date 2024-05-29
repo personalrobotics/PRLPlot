43f6b5a0e1d805e64ad7aca0f0ef2dabd3f7e13b
Abhay Deshpande <a.deshpande012@gmail.com>
2024-05-29 13:26:20 -0700

    Bugfixes and improvements

--- prlplot/plot.py
+++ prlplot/plot.py
@@ -1,87 +1,86 @@
-from typing import Any, Dict, Optional, Union
+from contextlib import contextmanager
+from typing import Optional, Union
+
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-from contextlib import contextmanager
+from matplotlib.transforms import Bbox
+
+from .util import collapse_dict
+
 
 FIG_SIZE = (7/4, 21/16)
 TEXT_SIZE = 7
-RC_CONFIG = {
+RC_CONFIG = collapse_dict({
     "font": {"size": TEXT_SIZE, "family": "Times New Roman"},
     "axes": {"titlesize": TEXT_SIZE, "labelsize": TEXT_SIZE},
     "xtick": {"labelsize": TEXT_SIZE},
     "ytick": {"labelsize": TEXT_SIZE},
     "legend": {"fontsize": TEXT_SIZE},
     "figure": {"titlesize": TEXT_SIZE, "figsize": FIG_SIZE},
-}
+})
 
-def _collapse_dict(d: Dict[str, Any], keys=None):
-    if keys is None:
-        keys = []
-    new_d = {}
-    for k, v in d.items():
-        keys.append(k)
-        if isinstance(v, dict):
-            new_d.update(_collapse_dict(v, keys))
-        else:
-            new_d[".".join(keys)] = v
-        keys.pop()
-    return new_d
 
 @contextmanager
 def figure(*args, **kwargs):
-    fig = create_figure(*args, **kwargs)
-    try:
+    with plt.rc_context(RC_CONFIG):
+        fig = plt.figure(*args, **kwargs)
         yield fig
-    finally:
         finalize_figure(fig)
 
-def create_figure(*args, **kwargs):
-    with plt.rc_context(_collapse_dict(RC_CONFIG)):
-        fig = plt.figure(*args, **kwargs)
-    return fig
 
 def finalize_figure(fig: Figure, style_axes=True):
-    fig.tight_layout(pad=0)
     if style_axes:
         for ax in fig.axes:
-            style_axes(ax)
+            finalize_axes(ax)
+    fig.tight_layout(pad=0)
 
-def style_axes(ax: Axes):
+
+def finalize_axes(ax: Axes):
     ax.spines[['right', 'top']].set_visible(False)
-    ax.set_xticks(ax.get_xlim())
-    ax.set_yticks(ax.get_ylim())
+
+    bbox = Bbox.union([line.get_bbox() for line in ax.get_lines()])
+    ax.set_xticks([bbox.xmin, bbox.xmax])
+    ax.set_yticks([bbox.ymin, bbox.ymax])
+
     ax.set_xlabel(ax.get_xlabel(), labelpad=-ax.xaxis.label.get_fontsize())
     ax.set_ylabel(ax.get_ylabel(), labelpad=-ax.yaxis.label.get_fontsize())
 
-def plot_kwargs(baseline: Optional[Union[str, int]]=None):
+
+def plot_kwargs(baseline: Optional[Union[str, int]] = None, **kwargs):
     if baseline is None:
         color = "#d00"
     elif isinstance(baseline, str):
         color = baseline
     else:
         color = ["#b3cfff"][baseline]
-    kwargs = {
+    new_kwargs = {
         "color": color,
         "clip_on": False,
-        "zorder": 3
+        "zorder": 4 if baseline is None else 3,
+        **kwargs
     }
-    return kwargs
+    return new_kwargs
 
-def line_plot_kwargs(baseline: Optional[Union[str, int]]=None):
-    kwargs = plot_kwargs(baseline)
-    kwargs.update({
+
+def line_plot_kwargs(baseline: Optional[Union[str, int]] = None, **kwargs):
+    new_kwargs = plot_kwargs(baseline)
+    new_kwargs.update({
         "markersize": 2,
-        "markeredgecolor": kwargs["color"],
-        "markerfacecolor": "#fff",
+        "markeredgecolor": new_kwargs["color"],
         "markeredgewidth": 2,
         "linewidth": 2,
+        "marker": "o"
     })
-    return kwargs
+    new_kwargs.update(kwargs)
+    return new_kwargs
+
 
 def savefig_png(fig: Figure, path: str, *args, **kwargs):
-    kwargs_ = {"transparent": True, "pad_inches": 0, "bbox_inches": "tight", "dpi": 300, **kwargs}
+    kwargs_ = {"transparent": True, "pad_inches": 0,
+               "bbox_inches": "tight", "dpi": 300, **kwargs}
     fig.savefig(path, *args, **kwargs_)
+
 
 def savefig_pdf(fig: Figure, path: str, *args, **kwargs):
     kwargs_ = {"transparent": True, "format": "pdf", **kwargs}
---
+++ prlplot/util.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict
+
+
+def collapse_dict(d: Dict[str, Any], keys=None):
+    if keys is None:
+        keys = []
+    new_d = {}
+    for k, v in d.items():
+        keys.append(k)
+        if isinstance(v, dict):
+            new_d.update(collapse_dict(v, keys))
+        else:
+            new_d[".".join(keys)] = v
+        keys.pop()
+    return new_d
