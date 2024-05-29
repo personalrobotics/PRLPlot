72d6a9b6b56d6e454162d11b0e008ef24fa947a8
Abhay Deshpande <a.deshpande012@gmail.com>
2024-05-29 12:52:05 -0700

    Improve gitignore and fix build

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/personalrobotics/PRLPlot",
-    packages=find_packages("prlplot"),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
